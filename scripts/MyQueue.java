26cf81210ee32788809a88b8f9213cfd1bfe310c
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2019-01-14 18:41:45 -0800

    Added add method to MyQueue

--- scripts/MyQueue.java
+++ scripts/MyQueue.java
@@ -14,4 +14,13 @@ class MyQueue<T> {
             this.next = null;
         }
     }
+    
+    // Add an item to the end of the list
+    public void add (T data) {
+        QueueNode<T> new_node = new QueueNode<T>(data);
+        if (last != null) last.next = new_node;
+        last = new_node;
+        if (first == null) first = last;
+        return;
+    }
 }
