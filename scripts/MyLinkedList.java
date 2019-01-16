71e9119e6a21cd0650357bd925762104b2a39c13
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2019-01-15 19:59:34 -0800

    Added appendAtLocation and appendtToTail to MyLinkedList

--- scripts/MyLinkedList.java
+++ scripts/MyLinkedList.java
@@ -15,13 +15,37 @@ class MyLinkedList<T> {
     }
     
     /*
-    Appen functions
+    Append functions
      */
     
-    void appendToHead (T data) {
+    void appendToHead(T data) {
         LinkedListNode<T> new_node = new LinkedListNode<T>(data);
         new_node.next = head;
         head = new_node;
+        return;
+    }
+    
+    // Appends new node at location after prev_node
+    void appendAtLocation(LinkedListNode<T> prev_node, T data) {
+        if (prev_node == null) return;
+        LinkedListNode<T> new_node = new LinkedListNode<T>(data);
+        new_node.next = prev_node.next;
+        prev_node.next = new_node;
+        return;
+    }
+    
+    void appendToTail (T data) {
+        LinkedListNode<T> new_node = new LinkedListNode<T>(data);
+        if (head == null) { // empty list
+            head = new_node;
+            return;
+        }
+        new_node.next = null;
+        LinkedListNode<T> last = head;
+        while (last.next != null) { // find last node in list
+            last = last.next;
+        }
+        last.next = new_node;
         return;
     }
 }
